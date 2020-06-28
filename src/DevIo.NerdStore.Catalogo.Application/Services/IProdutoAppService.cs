cc4d2c02d33f6fb572a1eb02e27a94a99e98a563
Giovanni Sacchitiello <bold012@einstein.br>
2020-06-28 20:29:59 -0300

    adicionando camada application utilizando automapper

---
+++ src/DevIo.NerdStore.Catalogo.Application/Services/IProdutoAppService.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DevIo.NerdStore.Catalogo.Application.ViewModels;
+
+namespace DevIo.NerdStore.Catalogo.Application.Services
+{
+    public interface IProdutoAppService : IDisposable
+    {
+        Task<IEnumerable<ProdutoViewModel>> ObterPorCategoria(int codigo);
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+        Task<IEnumerable<CategoriaViewModel>> ObterCategorias();
+
+        Task AdicionarProduto(ProdutoViewModel produtoViewModel);
+        Task AtualizarProduto(ProdutoViewModel produtoViewModel);
+
+        Task<ProdutoViewModel> DebitarEstoque(Guid id, int quantidade);
+        Task<ProdutoViewModel> ReporEstoque(Guid id, int quantidade);
+    }
+}
